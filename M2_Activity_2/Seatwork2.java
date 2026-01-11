6608e2deb1ad280d361dc3e9c4ae48c7d0a6c2f1
Frendel B. Pulusan <fbpulusan@headoffice.corp.bpi.com.ph>
2026-01-11 11:28:11 +0800

    commit

---
+++ M2_Activity_2/Seatwork2.java
@@ -0,0 +1,8 @@
+public class SeatWork2 {
+    public static void main(String[] args) {
+
+        WhiteBoard wb = new WhiteBoard();
+        int result = wb.addNumbers(5, 3);
+        System.out.println("The total is: " + String.valueOf(result));
+    }
+}
