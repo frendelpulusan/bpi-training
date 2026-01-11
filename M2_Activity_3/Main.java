6b1b7008464bd5ce44a16cd1ad6f92ef62ef18fa
Frendel B. Pulusan <fbpulusan@headoffice.corp.bpi.com.ph>
2026-01-11 11:32:09 +0800

    commit

---
+++ M2_Activity_3/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Car car1 = new Car();
+        car1.displayCarInfo();
+
+        Car car2 = new Car("Toyota", "White", 2022);
+        car2.displayCarInfo();
+    }
+}
